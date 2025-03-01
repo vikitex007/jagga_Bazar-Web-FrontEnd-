e434135ec9caccbeae7a49d540938f21240438e0
vikitex007 <vikitex007@gmail.com>
2025-03-01 21:48:45 +0545

    minor changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -69,22 +69,22 @@ export default function Home() {
       </div>
 
       {/* swiper */}
-      <Swiper navigation>
-        {offerListings &&
-          offerListings.length > 0 &&
-          offerListings.map((listing) => (
-            <SwiperSlide>
-              <div
-                style={{
-                  background: `url(${listing.imageUrls[0]}) center no-repeat`,
-                  backgroundSize: 'cover',
-                }}
-                className='h-[500px]'
-                key={listing._id}
-              ></div>
-            </SwiperSlide>
-          ))}
-      </Swiper>
+      <Swiper navigation modules={[Navigation]}>
+  {offerListings &&
+    offerListings.length > 0 &&
+    offerListings.map((listing) => (
+      <SwiperSlide key={listing._id}>
+        <div
+          style={{
+            background: `url(${listing.imageUrls?.[0] || '/fallback-image.jpg'}) center no-repeat`,
+            backgroundSize: 'cover',
+          }}
+          className='h-[500px]'
+        ></div>
+      </SwiperSlide>
+    ))}
+</Swiper>
+
 
       {/* listing results for offer, sale and rent */}
 
